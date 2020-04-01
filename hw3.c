65a5ec00b0ed0b5035bdd7a180d055ea35b0839e
leelohoon <21600558@handong.edu>
2020-04-01 15:03:53 +0900

    a

--- hw3.c
+++ hw3.c
@@ -50,14 +50,14 @@ int main(int argc, char *argv[])
 
     printf("111111111111111");
 
-    pthread_attr_init(&sattr);
-    pthread_attr_init(&rattr);
-    printf("2222222222222222");
-    pthread_create(&stid, &sattr, sender, sqid);
-    pthread_create(&rtid, &rattr, receiver, rqid);
-    printf("333333333333333333");
-    pthread_join(stid, NULL);
-    pthread_join(rtid, NULL);
+    // pthread_attr_init(&sattr);
+    // pthread_attr_init(&rattr);
+    // printf("2222222222222222");
+    // pthread_create(&stid, &sattr, sender, sqid);
+    // pthread_create(&rtid, &rattr, receiver, rqid);
+    // printf("333333333333333333");
+    // pthread_join(stid, NULL);
+    // pthread_join(rtid, NULL);
 
     // msgctl(sqid, IPC_RMID, 0);
     // msgctl(rqid, IPC_RMID, 0);
