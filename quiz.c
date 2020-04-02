0acb3687d2c5661b2afc3d570bc6b41f4c59a964
leelohoon <21600558@handong.edu>
2020-04-02 09:19:27 +0900

    a

---
+++ quiz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+#define BUFFER_SIZE 1024
+
+
+int main(int argc, char *argv[]){
+
+    return 0;
+}
