e738af250597c918596e4ce7571c36e769093023
leelohoon <21600558@handong.edu>
2020-04-03 23:18:03 +0900

    a

--- hw3.c
+++ hw3.c
@@ -76,6 +76,7 @@ void *sender(void *param)
 
     while (strcmp(string_buffer, "quit") != 0)
     {
+        fflush(stdin);
         printf("[msg] ");
         fgets(string_buffer, BUFFER_SIZE, stdin);
 
